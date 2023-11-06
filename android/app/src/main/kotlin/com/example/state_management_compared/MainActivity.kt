7a2f778937d8fc9a529cb2bb43b5fc4ccceffda4
YashMakan <yashmakan.work@gmail.com>
2023-11-07 01:20:55 +0530

    ui done

---
+++ android/app/src/main/kotlin/com/example/state_management_compared/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.state_management_compared
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:state_management_compared/views/home_page/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram State Management App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
